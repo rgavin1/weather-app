60f2a75abccd0160ceab56d60d2f83df50ba827c
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2021-03-03 10:46:36 -0500

    Updated Weekly Forecast

--- src/Components/WeeklyForecast.js
+++ src/Components/WeeklyForecast.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWind, faCompass, faUmbrella } from '@fortawesome/free-solid-svg-icons'
+import { faWind, faUmbrella } from '@fortawesome/free-solid-svg-icons'
 
 const IconImage = ({ image }) => {
-    // const iconImage = image ? '//cdn.weatherapi.com/weather/64x64/day/113.png' : image;
-    return <img src={image.icon} alt={image.text} />        
+    return  <>
+                <img src={image.icon} alt={image.text} />
+                <h1>{image.text}</h1>
+            </>        
+}
+
+const Forecast = ({ forecast, location }) => {
+    const list = forecast.forecastday;
+    console.log(list);
+    return <>
+        <h1>{ location.name }</h1> 
+        <ul>
+            {
+                list 
+                ? list.map( (item, id) =>
+                    <li key={id} >
+                        <IconImage image={item.day.condition} />
+                        <FontAwesomeIcon icon={faUmbrella} />
+                        {item.day.daily_chance_of_rain}%
+                        <FontAwesomeIcon icon={faWind} />
+                        {item.day.maxwind_mph} mph     
+                    </li>
+                )
+                : ''
+            }
+        </ul>
+    </>
 }
 
 const WeeklyForecast = ({ location, current, forecast }) => {
-    
-    return <div>
-            <h1>{ location.name }</h1>  
-            <h2>{ current.temp_f }°F</h2> 
-            { 
-                current.condition 
-                ? <IconImage image={current.condition} />
-                : ''
-            } 
-            <FontAwesomeIcon icon={faUmbrella} />
-            {/* <h2>{ forecast.forecastday[0].day.daily_chance_of_rain }</h2>  */}
-            <FontAwesomeIcon icon={faWind} />
-            <h2>{ current.wind_mph }mph</h2> 
-            <FontAwesomeIcon icon={faCompass} />
-            <h2>{ current.wind_dir }</h2> 
-        </div>
+    console.log(forecast );
+    return  <div> 
+                <Forecast forecast={forecast} location={location} />
+            </div>
 }
 
 export default WeeklyForecast;
