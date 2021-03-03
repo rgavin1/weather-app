7730a5c5a3a4efd300fb5f8f0e9f0e1c84112fbf
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2021-03-02 20:02:14 -0500

    SearchBox is connected to the App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 // import './App.css';
 import SearchBox from './Containers/SearchBox';
 import WeeklyForecast from './Components/WeeklyForecast';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <SearchBox />
-      <WeeklyForecast />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { input: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+handleChange(event){
+
+}
+handleSubmit(event){
+    event.preventDefault();
+    alert("App.js");
+    // Make a call to the API
+}
+render(){
+    return (
+        <>
+          <SearchBox handleSubmit={this.handleSubmit} handleChange={this.handleChange} input={this.state.input} />
+          <WeeklyForecast />
+        </>
+    );
+}
 }
 
 export default App;
--- src/Containers/SearchBox.js
+++ src/Containers/SearchBox.js
@@ -1,26 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SearchBox extends Component {
-    constructor(props){
-        super(props);
-        this.state = { input: '' };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange(event){
-        this.setState({ input: event.target.value })
-    }
-    handleSubmit(event){
-        event.preventDefault();
-    }
-    render(){
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <input type="text" value={this.state.input} onChange={this.handleChange} />
+const SearchBox = ({ handleSubmit, handleChange, input }) => {
+    return (
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={input} onChange={handleChange} />
                 <input type="submit" value="find" />
             </form>
-        );
-    }
+    );
 }
 
 export default SearchBox;
