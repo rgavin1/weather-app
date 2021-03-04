dc936fb7cfe8785b075b31abe8d77756ccdc2621
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2021-03-04 09:02:14 -0500

    Added NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,15 +5,21 @@ import { faCloudSunRain } from '@fortawesome/free-solid-svg-icons'
 const NavBar = () => {
     return (
     <div className="navbar" >
-        <div className="navbar__logo">
-                <FontAwesomeIcon icon={faCloudSunRain} />
-        </div> 
-        <div className="navbar__menu">
-            <ul>
-                <li>Home</li>
-                <li>Photos</li>
-                <li>Contact</li>
-            </ul>
+        <div className="container">
+            <div className="grid align-center">
+                <div className="col- navbar__container" >
+                    <div className="navbar__logo">
+                        <FontAwesomeIcon icon={faCloudSunRain} />
+                    </div> 
+                    <div className="navbar__menu">
+                        <ul>
+                            <li>Home</li>
+                            <li>Photos</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     );
