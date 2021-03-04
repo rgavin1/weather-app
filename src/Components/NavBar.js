cecafcf1d7fb9f01c6e4b47aae29779ff42dc2b3
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2021-03-04 08:44:23 -0500

    Added reflexgrid file

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloudSunRain } from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
-    return (<div className="navbar" ></div>)
+    return (
+    <div className="navbar" >
+        <div className="navbar__logo">
+                <FontAwesomeIcon icon={faCloudSunRain} />
+        </div> 
+        <div className="navbar__menu">
+            <ul>
+                <li>Home</li>
+                <li>Photos</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    </div>
+    );
 }   
 
 export default NavBar; 
